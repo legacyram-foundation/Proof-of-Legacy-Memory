6822d03821928d3d459f7bd2dbbd56bc12bbfa2c
Your Name <you@example.com>
2026-03-18 05:18:15 -0300

    fix: reduce initial mainnet difficulty to 4 for genesis mining

--- polm.py
+++ polm.py
@@ -453,7 +453,7 @@ class Blockchain:
         self.tx_block:  Dict[int, List[str]] = {}
         self.ledger     = Ledger()
         self.mempool    = Mempool()
-        self._diff      = T_DIFF if testnet else 5
+        self._diff      = T_DIFF if testnet else 4
         self._peers:    Set[str] = set()
         self._lock      = threading.Lock()
         self._load()
